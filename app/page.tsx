b20f7da2d5bb9c60b0e50883e050077c35a31499
r0ckwav3 <peterrockwave@gmail.com>
2024-07-01 16:23:18 -0400

    more work on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,74 @@
 import Image from "next/image";
 import aboutjson from "@/app/data/about.json";
+import contactjson from "@/app/data/contact.json";
+import projectsjson from "@/app/data/projects.json";
 
 export default function Home() {
   return (
-    <HomeBanner />
+    <div>
+      <HomeBanner />
+      <ProjectSummaries />
+    </div>
   );
 }
 
 function HomeBanner() {
+  const contact_text = contactjson.methods.map((e) => e.address).join(" | ");
+
   return (
     <div className="flex flex-row m-8 p-8 bg-slate-700">
-      <div className="flex-none mr-8">
+      <div className="w-48 h-72 relative flex-none mr-8">
         <Image
+          style={{ objectFit: "cover" }}
           src={aboutjson.image.src}
           alt={aboutjson.image.alt}
-          width={200}
-          height={300}
+          fill
         />
-      </div >
-      <div className="flex place-content-between text-lg text-white font-light">
-        <div> {aboutjson.short_about} </div>
-        <div> contact </div>
       </div>
-    </div >
-  )
+      <div className="flex flex-col place-content-between text-lg text-white font-light">
+        <div> {aboutjson.short_about} </div>
+        <div className="text-center"> {contact_text} </div>
+      </div>
+    </div>
+  );
+}
+
+type Project = {
+  title: string;
+  image: { src: string; alt: string };
+  short_desc: string;
+  long_desc: string;
+  links: Array<{ name: string; url: string }>;
+};
+
+function ProjectSummaries() {
+  return (
+    <div className="flex flex-col items-center">
+      {projectsjson.projects.map((proj, i) => (
+        <ProjectSummary project={proj} key={proj.title} />
+      ))}
+    </div>
+  );
+}
+
+function ProjectSummary({ project }: Readonly<{ project: Project }>) {
+  return (
+    <div className="flex flex-row w-1/2 m-8">
+      <div className="w-48 h-48 relative flex-none">
+        <Image
+          style={{ objectFit: "cover" }}
+          src={project.image.src}
+          alt={project.image.alt}
+          fill
+        />
+      </div>
+      <div className="m-8">
+        <div className="text-black text-2xl font-bold">
+          <a href={project.links[0].url}>{project.title}</a>
+        </div>
+        <br />
+        <div className="text-lg">{project.short_desc}</div>
+      </div>
+    </div>
+  );
 }
