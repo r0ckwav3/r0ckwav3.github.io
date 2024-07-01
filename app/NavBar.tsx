226ca498014256cba0306176f3541569c5ac666c
r0ckwav3 <peterrockwave@gmail.com>
2024-07-01 16:18:51 -0400

    formatting

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,22 +1,25 @@
-export default function NavBar(){
+export default function NavBar() {
   return (
-    <div className="min-w-screen h-24 sticky top-0 flex flex-row items-center bg-cyan-600">
+    <div className="min-w-screen h-24 sticky top-0 flex flex-row items-center z-40 bg-cyan-600">
       <div className="grow-0 w-8" />
-      <NavBarButton name="Home" link = "/" />
+      <NavBarButton name="Home" link="/" />
       <div className="grow" />
-      <NavBarButton name="About Me" link = "/about" />
-      <NavBarButton name="Projects" link = "/projects" />
-      <NavBarButton name="Research" link = "/research" />
-      <NavBarButton name="Contact" link = "/contact" />
+      <NavBarButton name="About Me" link="/about" />
+      <NavBarButton name="Projects" link="/projects" />
+      <NavBarButton name="Research" link="/research" />
+      <NavBarButton name="Contact" link="/contact" />
       <div className="grow-0 w-8" />
-    </div  >
-  )
+    </div>
+  );
 }
 
-function NavBarButton({ name, link }: Readonly<{name: string, link: string}>){
+function NavBarButton({
+  name,
+  link,
+}: Readonly<{ name: string; link: string }>) {
   return (
     <div className="m-4 font-light text-xl text-slate-200">
-      <a href={link}> {name} </a >
+      <a href={link}> {name} </a>
     </div>
-  )
+  );
 }
